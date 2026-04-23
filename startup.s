b222bdf092116074f947aa625c57fce9842253a7
Sumeyya Aktas <celestinedior1225@gmail.com>
2026-04-23 20:46:36 +0300

    add bss loop

--- startup.s
+++ startup.s
@@ -8,3 +8,14 @@ _start:
     LDR r0, =_bss_start
     LDR r1, =_bss_end
     MOV r2, #0
+
+bss_loop:
+    CMP r0, r1
+    BGE bss_done
+    STR r2, [r0], #4
+    B bss_loop
+
+bss_done:
+    LDR r0, =_data_flash_start
+    LDR r1, =_data_start
+    LDR r2, =_data_end
