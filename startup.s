b4a9ebe328f15bfa594bbd933784cad1e086f3ce
Sumeyya Aktas <celestinedior1225@gmail.com>
2026-04-24 18:49:49 +0300

    add data loop

--- startup.s
+++ startup.s
@@ -19,3 +19,16 @@ bss_done:
     LDR r0, =_data_flash_start
     LDR r1, =_data_start
     LDR r2, =_data_end
+
+data_loop:
+    CMP r1, r2
+    BGE data_done
+    LDR r3, [r0], #4
+    STR r3, [r1], #4
+    B data_loop
+
+data_done:
+    BL main
+
+hang:
+    B hang
