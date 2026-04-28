28a4c834f1675a42c62d6c349217fdc5c6658982
Sumeyya Aktas <celestinedior1225@gmail.com>
2026-04-28 19:53:06 +0300

    initialize uart

---
+++ src/uart/pl011.c
@@ -0,0 +1,15 @@
+#include "uart/pl011.h"
+
+void uart_init(void)
+{
+    UART0->CR = 0;
+
+    while (UART0->FR & FR_BUSY);
+    
+    UART0->LCRH &= ~LCRH_FEN;
+    UART0->IBRD = UART_IBRD;
+    UART0->FBRD = UART_FBRD;
+    UART0->LCRH = LCRH_WLEN_8 | LCRH_FEN;
+    UART0->ICR = 0x7FFU;
+    UART0->CR = CR_UARTEN | CR_TXE | CR_RXE;
+}
