e3d2be7ea38ca0a03e9e6d5bddc852e3e61cd621
Sumeyya Aktas <celestinedior1225@gmail.com>
2026-05-04 22:47:19 +0300

    implement uart put char/str

--- src/uart/pl011.c
+++ src/uart/pl011.c
@@ -13,3 +13,34 @@ void uart_init(void)
     UART0->ICR = 0x7FFU;
     UART0->CR = CR_UARTEN | CR_TXE | CR_RXE;
 }
+
+void uart_putc(char c)
+{
+    while (UART0->FR & FR_TXFF);
+    UART0->DR = c;
+
+    if (c == '\n')
+    {
+        uart_putc('\r');
+    }
+}
+
+void uart_puts(const char *s)
+{
+    while (*s)
+    {
+        uart_putc(*s++);
+    }
+}
+
+void print_uint32(uint32_t n)
+{
+    if (n < 10)
+    {
+        uart_putc('0' + n);
+        return;
+    }
+
+    print_uint32(n / 10);
+    uart_putc('0' + (n % 10));
+}
