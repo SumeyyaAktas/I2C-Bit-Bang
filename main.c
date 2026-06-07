e04873cec81491eefaa67a421777ed2951c60abf
Sumeyya Aktas <celestinedior1225@gmail.com>
2026-06-07 21:30:51 +0300

    verify gpio r-m-w logic is working

--- main.c
+++ main.c
@@ -1,9 +1,20 @@
 #include "uart/pl011.h"
+#include "gpio/gpio.h"
 
 void main(void)
 {
     uart_init();
-    uart_puts("\nHello!\n");
+
+    gpio_init();
+    
+    gpio_write(SCL_PIN, 0);
+    gpio_write(SDA_PIN, 0);
+
+    gpio_read(SCL_PIN);
+    gpio_read(SDA_PIN);
+
+    print_uint32(gpio_read(SCL_PIN));
+    print_uint32(gpio_read(SDA_PIN));
 
     __asm__ volatile ("hlt");
 }
