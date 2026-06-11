d74cd12795ea62f1e77204f6a611be7c960e0485
Sumeyya Aktas <celestinedior1225@gmail.com>
2026-06-11 19:04:27 +0300

    implement write bit helper

--- src/i2c/i2c.c
+++ src/i2c/i2c.c
@@ -35,3 +35,15 @@ void i2c_stop(void)
     gpio_write(SDA_PIN, 1);
     i2c_delay();
 }
+
+static void i2c_write_bit(uint8_t bit)
+{
+    gpio_write(SDA_PIN, bit);
+    i2c_delay();
+
+    gpio_write(SCL_PIN, 1);
+    i2c_delay();
+
+    gpio_write(SCL_PIN, 0);
+    i2c_delay();
+}
