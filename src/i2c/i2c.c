e62dc9571c6d70bb92bfe0e82e261ba4bd441ee2
Sumeyya Aktas <celestinedior1225@gmail.com>
2026-06-12 22:38:55 +0300

    add i2d read bit helper

--- src/i2c/i2c.c
+++ src/i2c/i2c.c
@@ -47,3 +47,20 @@ static void i2c_write_bit(uint8_t bit)
     gpio_write(SCL_PIN, 0);
     i2c_delay();
 }
+
+static uint8_t i2c_read_bit(void)
+{
+    gpio_set_input(SDA_PIN);
+    i2c_delay();
+
+    gpio_write(SCL_PIN, 1);
+    i2c_delay();
+
+    uint8_t bit = gpio_read(SDA_PIN);
+
+    gpio_write(SCL_PIN, 0);
+    gpio_set_output(SDA_PIN);
+    i2c_delay();
+
+    return bit;
+}
